8a2099f59356c3887585381ad67c68e0b155b783
Doyun <materokatti@gmail.com>
2024-03-13 20:10:16 +0900

    ADD : chanage intro

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,17 +3,27 @@ import { CMS_NAME } from "@/lib/constants";
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="flex flex-start text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        <img
+          className="inline-block size-[3rem]"
+          src="/favicon/logo.png"
+          alt="Devstory logo"
+        />
         DevStory
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         <a
+          href="https://www.linkedin.com/in/doyun-hwang-397855214/"
+          className="underline hover:text-blue-600 duration-200 transition-colors"
+        >
+          LinkedIn
+        </a>{" "}
+        <a
           href="https://www.youtube.com/channel/UCNF2FGsT3y6Xvbqv_9KJ0pA"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          DevStory
-        </a>{" "}
-        blog
+          Utube
+        </a>
       </h4>
     </section>
   );
